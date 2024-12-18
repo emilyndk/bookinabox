1e0fe590d847a284bdf620099b8186884458f35f
Emily Nielsen <emily.nielsen09@gmail.com>
2024-12-18 18:54:58 +0100

    yaaay

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,6 +21,22 @@ export default {
        
 
 		extend: {
+
+
+
+      keyframes: {
+        typewriter: {
+          'from': { width: '0%' },
+          'to': { width: '100%' },
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+        },
+      },
+      animation: {
+        typewriter: 'typewriter 3s steps(30, end), blink .5s step-end infinite',
+      },
+    
             opacity:{
                 50: '.50',   
    
